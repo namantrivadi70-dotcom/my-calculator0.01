712d766e72b2d1173e69a038d1a5699abe0340f1
namantrivadi70-dotcom <namantrivadi70@gmail.com>
2026-02-14 19:17:27 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,26 @@
+let input = document.getElementById('inputbox');
+let button = document.querySelectorAll('button');
+let string="";
+let arr= Array.from(buttons);
+arr.forEach(button=>{
+    button.addeventlistener('click',(e)=>{
+        if(e.target.innerHTML == '='){
+            string = eval(string);
+            input.value=string;
+        }
+        else if(e.target.innerHTML == 'AC'){
+            string ="";
+            input.value = string;
+        }
+        else if(e.target.innerHTML =='DEL'){
+            string = string.substring(0, string.length-1);
+            input.value = string;
+        }
+        else{
+        string += e.target.innerhtml;
+        input.value = string;
+        }
+
+
+    })
+})
